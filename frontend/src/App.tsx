e7c7cd359aa40536d98711b8b26c2db716025055
Bolshialex <145608606+Bolshialex@users.noreply.github.com>
2026-05-29 16:19:29 -0700

    Revert "Add styling changes and adding search form and upload features"
    This reverts commit d51ea6ef271ec85a56a834b452b1020e729277df.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,12 @@
 import "./App.css";
-import Main from "./layout/Main";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import Upload from "./pages/Upload";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route element={<Main />}>
-          <Route path="/" element={<Home />} />
-          <Route path="/upload" element={<Upload />} />
-        </Route>
-      </Routes>
-    </Router>
+    <>
+      <div>
+        <h1>Hello</h1>
+      </div>
+    </>
   );
 }
 
